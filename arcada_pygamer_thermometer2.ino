2102bf13dd04a27c8270985578dd72ae264d5609
Jozsef Frigo <frigojozsef@gmail.com>
2022-01-15 20:47:56 +0100

    input handling

--- arcada_pygamer_thermometer2.ino
+++ arcada_pygamer_thermometer2.ino
@@ -11,10 +11,12 @@ Adafruit_Arcada arcada;
 extern Adafruit_SPIFlash Arcada_QSPI_Flash;
 uint32_t buttons, last_buttons;
 uint8_t j = 0;  // neopixel counter for rainbow
+#define JOY_THRESHOLD 100
 
 //---- brightness ----
 #define BR_SIZE 12
 int brightness_table[BR_SIZE] = {1, 2, 3, 5, 9, 14, 22, 36, 59, 95, 155, 250};
+int led_brightness_table[BR_SIZE] = {0, 14, 19, 25, 33, 45, 60, 80, 107, 143, 191, 255};
 
 //---- DS18B20 ----
 #define ONE_WIRE_BUS 3
@@ -32,12 +34,16 @@ uint32_t PX_CYAN = arcada.pixels.Color(0,1,1);
 uint32_t PX_BLUE = arcada.pixels.Color(0,0,1);
 
 //---- data to display ----
-int countdown = 59;
+int countdown = -1;
 int t_set = 21;
 AverageTemp avgTemp;
-int time_interval = 60;
+int time_interval = 1;
 int lcd_brightness = 9;
 int led_brightness = 9;
+
+//---- timing ---
+#define INTERVALS_SIZE 5
+int intervals[INTERVALS_SIZE] = {30, 60, 120, 180, 300};
 
 //---- timer callback ----
 // Check the timer callback, this function is called every second!
@@ -104,90 +110,109 @@ void setup() {
   arcada.display->fillScreen(ARCADA_BLACK);
 }
 
+void processInput(int x, int y) {
+  uint32_t buttons = arcada.readButtons();
+  if (buttons & ARCADA_BUTTONMASK_A && time_interval < INTERVALS_SIZE - 1) {
+    time_interval++;
+  }
+  if (buttons & ARCADA_BUTTONMASK_B && time_interval > 0) {
+    time_interval--;
+  }
+  if (buttons & ARCADA_BUTTONMASK_START) {
+    t_set++;
+  }
+  if (buttons & ARCADA_BUTTONMASK_SELECT) {
+    t_set--;
+  }
+  if (x > JOY_THRESHOLD && lcd_brightness < BR_SIZE - 1) {
+    lcd_brightness++;
+  }
+  if (x < -JOY_THRESHOLD && lcd_brightness > 0) {
+    lcd_brightness--;
+  }
+  if (y > JOY_THRESHOLD && led_brightness < BR_SIZE - 1) {
+    led_brightness++;
+  }
+  if (y < -JOY_THRESHOLD && led_brightness > 0) {
+    led_brightness--;
+  }
+  last_buttons = buttons;
+}
+
 void loop() {
-  delay(100);  // add some delay so our screen doesnt flicker
+  // delay(100);  // add some delay so our screen doesnt flicker
   bool playsound = false;
+  int x = arcada.readJoystickX();
+  int y = arcada.readJoystickY();
+
+  processInput(x, y);
+  arcada.setBacklight(brightness_table[lcd_brightness]);
 
   // first line
   arcada.display->fillRect(0, 0, 128, 16, ARCADA_BLACK);
   arcada.display->setTextColor(ARCADA_GREEN);
   arcada.display->setCursor(0, 4);
-  arcada.display->printf("%3d", countdown);
-  arcada.display->print("s   ");
+  if (countdown >= 0) {
+    arcada.display->printf("%3d", countdown);
+    arcada.display->print("s");
+  } else {
+    arcada.display->print("OK");
+  }
   arcada.display->setCursor(52, 4);
   arcada.display->print(t_set);
-  arcada.display->print("\xF8" "C ");
+  arcada.display->print("\xF8" "C");
   // Read battery
   arcada.display->setCursor(96, 4);
   float vbat = arcada.readBatterySensor();
   arcada.display->print(vbat); arcada.display->println("V");
 
-  // average temperature
-  printTemperature();
+  // upper darkgreen row
+  arcada.display->fillRect(0, 22, 128, 10, ARCADA_BLACK);
+  arcada.display->setTextColor(ARCADA_DARKGREEN);
+  // current temperature
+  arcada.display->setCursor(52, 22);
+  arcada.display->print(avgTemp.temp_curr, 2);
 
-  // darkgreen row
+  // lower darkgreen row
   arcada.display->fillRect(0, 126, 128, 10, ARCADA_BLACK);
   arcada.display->setTextColor(ARCADA_DARKGREEN);
   // Read light sensor
   arcada.display->setCursor(0, 126);
-  arcada.display->printf("L%d   ", arcada.readLightSensor());
-  // current temperature
-  arcada.display->setCursor(96, 126);
-  arcada.display->print(avgTemp.temp_curr, 2);
+  arcada.display->printf("L%d", arcada.readLightSensor());
+  // joystick
+  arcada.display->setCursor(40, 126);
+  arcada.display->printf("x:%d", x);
+  arcada.display->setCursor(90, 126);
+  arcada.display->printf("y:%d", y);
 
   // last row
   arcada.display->fillRect(0, 148, 128, 10, ARCADA_BLACK);
   arcada.display->setTextColor(ARCADA_GREEN);
   arcada.display->setCursor(0, 148);
-  arcada.display->printf("%3ds", time_interval);
+  arcada.display->printf("%3ds", intervals[time_interval]);
   arcada.display->setCursor(40, 148);
-  arcada.display->printf("lcd:%2d ", lcd_brightness);
+  arcada.display->printf("lcd:%2d", lcd_brightness + 1);
   arcada.display->setCursor(90, 148);
-  arcada.display->printf("led:%2d ", led_brightness);
+  arcada.display->printf("led:%2d", led_brightness);
 
   // LEDs
-  arcada.pixels.setPixelColor(0, PX_BLUE * brightness_table[led_brightness]);
-  arcada.pixels.setPixelColor(1, PX_CYAN * brightness_table[led_brightness]);
-  arcada.pixels.setPixelColor(2, PX_GREEN * brightness_table[led_brightness]);
-  arcada.pixels.setPixelColor(3, PX_YELLOW * brightness_table[led_brightness]);
-  arcada.pixels.setPixelColor(4, PX_RED * brightness_table[led_brightness]);
+  arcada.pixels.setPixelColor(0, PX_BLUE * led_brightness_table[led_brightness]);
+  arcada.pixels.setPixelColor(1, PX_CYAN * led_brightness_table[led_brightness]);
+  arcada.pixels.setPixelColor(2, PX_GREEN * led_brightness_table[led_brightness]);
+  arcada.pixels.setPixelColor(3, PX_YELLOW * led_brightness_table[led_brightness]);
+  arcada.pixels.setPixelColor(4, PX_RED * led_brightness_table[led_brightness]);
   arcada.pixels.show();
-
-  /*
-  uint8_t pressed_buttons = arcada.readButtons();
-  arcada.display->fillRect(0, 70, 160, 60, ARCADA_BLACK);
-  
-  if (pressed_buttons & ARCADA_BUTTONMASK_A) {
-    Serial.print("A");
-    arcada.display->drawCircle(145, 100, 10, ARCADA_WHITE);
-  }
-  if (pressed_buttons & ARCADA_BUTTONMASK_B) {
-    Serial.print("B");
-    arcada.display->drawCircle(120, 100, 10, ARCADA_WHITE);
-  }
-  if (pressed_buttons & ARCADA_BUTTONMASK_SELECT) {
-    Serial.print("Sel");
-    arcada.display->drawRoundRect(60, 100, 20, 10, 5, ARCADA_WHITE);
-  }
-  if (pressed_buttons & ARCADA_BUTTONMASK_START) {
-    Serial.print("Sta");
-    arcada.display->drawRoundRect(85, 100, 20, 10, 5, ARCADA_WHITE);
-  }
-
-  arcada.display->drawCircle(20, 100, 20, ARCADA_WHITE);
-  int joyX = arcada.readJoystickX() / 512.0 * 15.0 + 20; 
-  int joyY = arcada.readJoystickY() / 512.0 * 15.0 + 100; 
-  arcada.display->fillCircle(joyX, joyY, 5, ARCADA_WHITE);
-  Serial.println();
-  */
-
-  last_buttons = buttons;
 
   if (playsound) {
     arcada.enableSpeaker(true);
     play_tune(audio, sizeof(audio));
     arcada.enableSpeaker(false);
   }
+
+  // average temperature
+  arcada.display->setTextColor(ARCADA_GREEN);
+  printTemperature(); // takes 750ms, including measurement
+
 }
 
 void play_tune(const uint8_t *audio, uint32_t audio_length) {
